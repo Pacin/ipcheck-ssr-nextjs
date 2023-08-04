eae6d246b0ece60f59c9a9a7aaa28189dcd3b561
Alper Pacin <alper@lotusflow.com>
2023-08-04 16:41:46 +0300

    fix

--- pages/api/check.js
+++ pages/api/check.js
@@ -3,32 +3,34 @@ const fs = require("fs");
 const Reader = require("@maxmind/geoip2-node").Reader;
 
 export default function handler(req, res) {
-  const { ip } = req.body;
-  // Synchronous database opening
-  let response = null;
+  if (req.method === "POST") {
+    const { ip } = req.body;
+    // Synchronous database opening
+    let response = null;
 
-  const dbBuffer = fs.readFileSync("./assets/GeoLite2-Country.mmdb");
+    const dbBuffer = fs.readFileSync("./assets/GeoLite2-Country.mmdb");
 
-  // This reader object should be reused across lookups as creation of it is
-  // expensive.
-  const reader = Reader.openBuffer(dbBuffer);
-  try {
-    response = reader.country(ip);
-  } catch (error) {
-    response = {
-      code: "US",
-      country: COUNTRIES["US"].countryName,
-      currency: COUNTRIES["US"].currency,
-      symbol: COUNTRIES["US"].symbol,
-    };
+    // This reader object should be reused across lookups as creation of it is
+    // expensive.
+    const reader = Reader.openBuffer(dbBuffer);
+    try {
+      response = reader.country(ip);
+    } catch (error) {
+      response = {
+        code: "US",
+        country: COUNTRIES["US"].countryName,
+        currency: COUNTRIES["US"].currency,
+        symbol: COUNTRIES["US"].symbol,
+      };
 
-    return res.status(200).json(response);
+      return res.status(200).json(response);
+    }
+
+    res.status(200).json({
+      code: response.country.isoCode,
+      country: COUNTRIES[response.country.isoCode].countryName,
+      currency: COUNTRIES[response.country.isoCode].currency,
+      symbol: COUNTRIES[response.country.isoCode].symbol,
+    });
   }
-
-  res.status(200).json({
-    code: response.country.isoCode,
-    country: COUNTRIES[response.country.isoCode].countryName,
-    currency: COUNTRIES[response.country.isoCode].currency,
-    symbol: COUNTRIES[response.country.isoCode].symbol,
-  });
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import JsonView from "react18-json-view";
 
-export default function Home({ ip, data }) {
+export default function Home({ ip }) {
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
@@ -10,10 +10,13 @@ export default function Home({ ip, data }) {
       const response = await fetch("/api/check", {
         method: "POST",
         body: JSON.stringify({ ip: userIp }),
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
 
       const data = await response.json();
-      return setUserData(data);
+      setUserData(data);
     };
 
     fetchData(ip);
@@ -22,7 +25,7 @@ export default function Home({ ip, data }) {
   return (
     <>
       <Head>
-        <title>Example IP Checker with SSR</title>
+        <title>Example IP Checker</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -41,7 +44,7 @@ export default function Home({ ip, data }) {
   );
 }
 
-export async function getServerSideProps({ req, res }) {
+export async function getServerSideProps({ req }) {
   let ip = req.headers["x-real-ip"];
   if (!ip) {
     const forwardedFor = req.headers["x-forwarded-for"];
